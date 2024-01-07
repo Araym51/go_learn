028bc67aa87f117f331ffac07a884e8ea5608237
Egor Ostroumov <araimo@yandex.ru>
2024-01-07 10:55:25 +0300

    added examples

--- functions_examples.go
+++ functions_examples.go
@@ -8,20 +8,39 @@ func divid(x, y int) (divide int) {
 	return //компилятор найдет указзаную переменную divide и вернет её без явного указания	
 }
 
+
+
+func calc(x, y int, action(x, y int) int)int{
+	return action(x, y)
+}
+
+// возврат функции из функции
+func createDivider(divider int) func(x int) int {
+	dividerFunc := func(x int) int {
+		return x / divider
+	}
+	return dividerFunc
+}
+
+
 func main() {
 	// объявление функции и вередача в переменную
 	var mult func(x, y int) int
 	mult = func(x, y int) int {return x * y}
 	res_mult := mult(2,3)
 	fmt.Println(res_mult)
+
+
 	// передача функции в переменную без объявления
 	summa := func (x, y int) int {return x + y}
 	res_sum := summa(2,3)
 	fmt.Println(res_sum)
 
 
-
-
-
+	fmt.Println("Вложенная функция:")
+	sumFunction := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(calc(2,3, sumFunc)) // функцию передаём без вызова!
 	fmt.Println("over")
 }
