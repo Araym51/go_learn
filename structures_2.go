96eb2086ea102140c77d4928cfe31d4bcf8a2a7e
Egor Ostroumov <araimo@yandex.ru>
2024-01-09 21:01:53 +0300

    interfaces 2 - begin

---
+++ structures_2.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// https://www.youtube.com/watch?v=Mi2mqnGm1h8&list=PLc2Vkg57qmuRNHp6NNvYRVgg3OP-b5E_v&index=14&ab_channel=ThisIsIT
+
+func main(){
+	fmt.Println("Interfaces 2")
+}
