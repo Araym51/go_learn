198c2158bc66358d90f64bc84e8fb23efe6a1e13
Egor Ostroumov <araimo@yandex.ru>
2024-01-18 19:47:47 +0300

    slices 2

--- arr_and_slices_2.go
+++ arr_and_slices_2.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
-/* https://www.youtube.com/watch?v=ecetBuEZ8oc&list=PLc2Vkg57qmuRNHp6NNvYRVgg3OP-b5E_v&index=16&ab_channel=ThisIsIT */ 
-func main(){
 
+/* https://www.youtube.com/watch?v=ecetBuEZ8oc&list=PLc2Vkg57qmuRNHp6NNvYRVgg3OP-b5E_v&index=16&ab_channel=ThisIsIT */ 
+
+func main(){
+	fmt.Println("Slices 2")
 }
