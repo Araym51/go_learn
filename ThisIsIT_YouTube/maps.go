885ef2cb42a1496186e6249dd02bd03f7aed500c
Egor Ostroumov <araimo@yandex.ru>
2024-01-23 19:58:17 +0300

    new folder

---
+++ ThisIsIT_YouTube/maps.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+/* https://www.youtube.com/watch?v=DTJ9Q-k0CEc&list=PLc2Vkg57qmuRNHp6NNvYRVgg3OP-b5E_v&index=19&ab_channel=ThisIsIT */ 
+
+func main(){
+	fmt.Println("maps")
+}
+
