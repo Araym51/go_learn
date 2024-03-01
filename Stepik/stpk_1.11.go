efee87e9098ff940d169f777568e4af191a4406c
Egor Ostroumov <araimo@yandex.ru>
2024-03-01 13:49:23 +0300

    arrs

--- Stepik/stpk_1.11.go
+++ Stepik/stpk_1.11.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	
+	var workArray [10]uint8
+	var a, b, i int
+	for fmt.Scan(&workArray[i]); i < 9; fmt.Scan(&workArray[i]){
+		i++
+	}
+	i = 0
+	for ;i < 3;{
+		fmt.Scan(&a, &b)
+		workArray[a], workArray[b] = workArray[b], workArray[a]
+		i ++
+	}
+	i = 0
+	for _, elem := range workArray {
+		fmt.Printf("%d ",elem)
+	}
 }
