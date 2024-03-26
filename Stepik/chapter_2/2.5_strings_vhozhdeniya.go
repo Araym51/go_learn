50b8a62070462044d5f1f546be493d4eaee947cb
Egor Ostroumov <araimo@yandex.ru>
2024-03-26 22:33:00 +0300

    strings vhozhd

--- Stepik/chapter_2/2.5_strings_vhozhdeniya.go
+++ Stepik/chapter_2/2.5_strings_vhozhdeniya.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
-
+import ("fmt"
+		"strings"
+		)
+/*
+check with "cordoba oba"
+*/
 func main(){
 	word_1, word_2 := "", ""
-	vhozhd := 0
-
+	fmt.Scanln(&word_1)
+	fmt.Scanln(&word_2)
+	if strings.Contains(word_1, word_2){
+		for _, i := range(word_2){
+			fmt.Println(strings.Index(word_1, string(i)))
+			break
+		}
+	} else {
+		fmt.Println(-1)
+	}
 }
